042faccb9ac635bdd63890627387ca37f6b227e1
Praveen Mathew <praveen.mathew92@outlook.com>
2019-10-19 12:56:30 +0530

    Mock InstitutionService with mockito | Fix #45

--- src/test/java/InstitutionAuthTests.java
+++ src/test/java/InstitutionAuthTests.java
@@ -1,31 +1,42 @@
 import helpers.InstitutionAuth;
 import models.Institution;
 import org.junit.Test;
+import org.mockito.Mockito;
+import services.InstitutionService;
 
 import static helpers.InstitutionAuth.encryptPassword;
 import static junit.framework.TestCase.*;
 
 public class InstitutionAuthTests {
-    private InstitutionAuth institutionAuth = new InstitutionAuth();
+    private static final String PASSWORD = "1234";
+    private static final String ENCRYPTED_PASSWORD = "28970aab014cf552b44cf480a4e9a9db47cfc1d6ecd29d8a23a8b4a0aba7395f33ca1695fde9f9" +
+            "dd36aa3f4eb4404c02a29454fb47632d68f16e26ffeb7bed42";
+
+    private InstitutionService institutionService = Mockito.mock(InstitutionService.class);
+
+    private InstitutionAuth institutionAuth = new InstitutionAuth(institutionService);
 
     @Test
     public void encyptPassword(){
-        String encryptedPassword = "28970aab014cf552b44cf480a4e9a9db47cfc1d6ecd29d8a23a8b4a0aba7395f33ca1695fde9f9" +
-                "dd36aa3f4eb4404c02a29454fb47632d68f16e26ffeb7bed42";
-        assertEquals(encryptedPassword, encryptPassword("1234"));
+        assertEquals(ENCRYPTED_PASSWORD, encryptPassword(PASSWORD));
     }
 
     @Test
     public void successLogin(){
         String institutionEmail = "etec@etec.com";
-        String institutionPassword = "12345";
-        assertTrue(institutionAuth.login(institutionEmail, institutionPassword));
+        Institution institution = new Institution("institution name", institutionEmail, ENCRYPTED_PASSWORD);
+        Mockito.when(institutionService.findByEmail(institutionEmail)).thenReturn(institution);
+
+        assertTrue(institutionAuth.login(institutionEmail, PASSWORD));
     }
 
     @Test
     public void failureLogin(){
         String institutionEmail = "etec@etec.com";
-        String institutionPassword = "1234";
-        assertFalse(institutionAuth.login(institutionEmail, institutionPassword));
+        String incorrectInstitutionPassword = "12345";
+        Institution institution = new Institution("institution name", institutionEmail, ENCRYPTED_PASSWORD);
+        Mockito.when(institutionService.findByEmail(institutionEmail)).thenReturn(institution);
+
+        assertFalse(institutionAuth.login(institutionEmail, incorrectInstitutionPassword));
     }
 }
